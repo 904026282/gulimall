8552c88be1fa3e0baffd6b68af7219159468db20
wangyangyang <904026282@qq.com>
2022-06-03 22:37:58 +0800

    逆向工程初始化完毕

--- gulimall-member/src/main/java/com/wyy/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/wyy/gulimall/member/GulimallMemberApplication.java
@@ -1,9 +1,11 @@
 package com.wyy.gulimall.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wyy.gulimall.member.dao")
 public class GulimallMemberApplication {
 
     public static void main(String[] args) {
